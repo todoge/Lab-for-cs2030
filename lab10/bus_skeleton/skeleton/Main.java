9d4924a7c6adf63fa02a6e6ff025a7470edc8e4c
Peh Jun Siang <austin9090@hotmail.com>
2020-04-22 02:57:09 +0800

    finish lab10

---
+++ lab10/bus_skeleton/skeleton/Main.java
@@ -0,0 +1,34 @@
+import java.time.Instant;
+import java.time.Duration;
+import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * This program finds different ways one can travel by bus (with a bit 
+ * of walking) from one bus stop to another.
+ *
+ * @author: Ooi Wei Tsang
+ * @version: CS2030 AY19/20 Semester 1, Lab 10
+ */
+public class Main {
+  /**
+   * The program read a sequence of (id, search string) from standard input.
+   * @param args Command line arguments
+   */
+  public static void main(String[] args) {
+    Instant start = Instant.now();
+      Scanner sc = new Scanner(System.in);
+      while (sc.hasNext()) {
+          BusStop srcId = new BusStop(sc.next());
+          String searchString = sc.next();
+          CompletableFuture<String> description = BusSg.findBusServicesBetween(srcId, searchString).thenCompose(x -> x.description());
+          CompletableFuture.allOf(description);
+          System.out.println(
+            description.join()
+          );
+      }
+      sc.close();
+      Instant stop = Instant.now();
+      System.out.printf("Took %,dms\n", Duration.between(start, stop).toMillis());
+  }
+}
